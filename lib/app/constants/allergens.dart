c283b9b0e08b9d984b4116a2610f4ebea6b0c3a4
Mabud <pavelalam401@gmail.com>
2024-12-03 13:23:16 +0530

    added icons and code for all allergens

---
+++ lib/app/constants/allergens.dart
@@ -0,0 +1,64 @@
+import 'package:cho_nun_btk/app/models/menu/menu.dart';
+
+List<Allergens> allergens = [
+  Allergens(
+      allergenId: '1',
+      allergenName: 'Gluten',
+      allergenImage: 'assets/svg/allergens/gluten.png'),
+  Allergens(
+      allergenId: '2',
+      allergenName: 'Dairy',
+      allergenImage: 'assets/svg/allergens/dairy.png'),
+  Allergens(
+      allergenId: '3',
+      allergenName: 'Eggs',
+      allergenImage: 'assets/svg/allergens/eggs.png'),
+  Allergens(
+      allergenId: '4',
+      allergenName: 'Nuts',
+      allergenImage: 'assets/svg/allergens/nuts.png'),
+  Allergens(
+      allergenId: '5',
+      allergenName: 'Peanuts',
+      allergenImage: 'assets/svg/allergens/peanuts.png'),
+  Allergens(
+      allergenId: '6',
+      allergenName: 'Shellfish',
+      allergenImage: 'assets/svg/allergens/shellfish.png'),
+  Allergens(
+      allergenId: '7',
+      allergenName: 'Fish',
+      allergenImage: 'assets/svg/allergens/fish.png'),
+  Allergens(
+      allergenId: '8',
+      allergenName: 'Soy',
+      allergenImage: 'assets/svg/allergens/soy.png'),
+  Allergens(
+      allergenId: '9',
+      allergenName: 'Sesame',
+      allergenImage: 'assets/svg/allergens/sesame.png'),
+  Allergens(
+      allergenId: '10',
+      allergenName: 'Mustard',
+      allergenImage: 'assets/svg/allergens/mustard.png'),
+  Allergens(
+      allergenId: '11',
+      allergenName: 'Celery',
+      allergenImage: 'assets/svg/allergens/celery.png'),
+  Allergens(
+      allergenId: '12',
+      allergenName: 'Lupin',
+      allergenImage: 'assets/svg/allergens/lupin.png'),
+  Allergens(
+      allergenId: '13',
+      allergenName: 'Sulfites',
+      allergenImage: 'assets/svg/allergens/sulfites.png'),
+  Allergens(
+      allergenId: '14',
+      allergenName: 'Corn',
+      allergenImage: 'assets/svg/allergens/corn.png'),
+  Allergens(
+      allergenId: '15',
+      allergenName: 'Bovine Proteins',
+      allergenImage: 'assets/svg/allergens/bovine_proteins.png'),
+];
