dda36fc717ea23b2bfa5060a9b8b6c2079ea184f
Mabud <pavelalam401@gmail.com>
2024-12-05 00:50:34 +0530

    Convert MenuView from Stateless to Stateful widget, implement category fetching, and enhance UI with editable category feature

--- lib/app/modules/Admin App/Menu/views/menu_view.dart
+++ lib/app/modules/Admin App/Menu/views/menu_view.dart
@@ -10,29 +10,47 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:readmore/readmore.dart';
 import 'package:sizer/sizer.dart';
 
-class MenuView extends StatelessWidget {
+class MenuView extends StatefulWidget {
   MenuView({super.key});
 
+  @override
+  State<MenuView> createState() => _MenuViewState();
+}
+
+class _MenuViewState extends State<MenuView> {
   FoodMenuController menuController = Get.put(FoodMenuController());
+
+  late Future<List<FoodCategory>> getCategories;
+
+  @override
+  void initState() {
+    super.initState();
+    getCategories = getAllCategories();
+  }
+
+  Future<List<FoodCategory>> getAllCategories() async {
+    return await menuController.getAllCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: Text(
-            "Manage Menu",
-            style: context.textTheme.titleLarge!.copyWith(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        // appBar: AppBar(
+        //   centerTitle: false,
+        //   title: Text(
+        //     "Manage Menu",
+        //     style: context.textTheme.titleLarge!.copyWith(
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
         body: Padding(
             padding: AppPading.containerPadding,
             child: FutureBuilder(
-                future: menuController.getAllCategories(),
+                future: getCategories,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Center(
@@ -50,7 +68,7 @@ class MenuView extends StatelessWidget {
 
                   return GetBuilder(builder: (FoodMenuController controller) {
                     return SingleChildScrollView(
-                      physics: NeverScrollableScrollPhysics(),
+                      // physics: NeverScrollableScrollPhysics(),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -100,7 +118,7 @@ class MenuView extends StatelessWidget {
                           ),
                           menuController.categories.length > 0
                               ? SizedBox(
-                                  height: 18.h,
+                                  height: 22.h,
                                   width: 100.w,
                                   child: ListView.builder(
                                       shrinkWrap: true,
@@ -145,17 +163,60 @@ class MenuView extends StatelessWidget {
                                                     isCircular: true,
                                                     borderWidth: 4.0,
                                                   ),
-                                                  Text(
-                                                    menuController
-                                                        .categories[index]
-                                                        .categoryName,
-                                                    style: context
-                                                        .textTheme.titleMedium!
-                                                        .copyWith(
-                                                      fontWeight:
-                                                          FontWeight.bold,
+                                                  SizedBox(
+                                                    width: 30.w,
+                                                    child: Text(
+                                                      menuController
+                                                          .categories[index]
+                                                          .categoryName,
+                                                      textAlign:
+                                                          TextAlign.center,
+                                                      maxLines: 1,
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                      style: context.textTheme
+                                                          .titleMedium!
+                                                          .copyWith(
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
                                                     ),
                                                   ),
+                                                  // Row(
+                                                  //   children: [
+                                                  //     Text("Edit"),
+                                                  //     Icon(Icons.edit)
+                                                  //   ],
+                                                  // )
+
+                                                  Container(
+                                                    decoration: BoxDecoration(
+                                                        color: AppColors
+                                                            .primaryLight,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(12)),
+                                                    child: IconButton(
+                                                      onPressed: () {
+                                                        Get.to(
+                                                                () =>
+                                                                    AddCategory(
+                                                                      category:
+                                                                          menuController
+                                                                              .categories[index],
+                                                                    ))!
+                                                            .then((value) {
+                                                          menuController
+                                                              .getAllCategories();
+                                                        });
+                                                      },
+                                                      icon: Icon(
+                                                        Icons.edit_outlined,
+                                                        color: AppColors
+                                                            .primaryDark,
+                                                      ),
+                                                    ),
+                                                  )
                                                 ],
                                               ),
                                             ),
@@ -183,6 +244,12 @@ class MenuView extends StatelessWidget {
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
+                              Text(
+                                "(${menuController.items.length.toString()})",
+                                style: context.textTheme.titleSmall!.copyWith(
+                                  color: AppColors.primaryLight,
+                                ),
+                              ),
                               Expanded(child: SizedBox()),
                               GestureDetector(
                                 onTap: () {
@@ -208,7 +275,78 @@ class MenuView extends StatelessWidget {
                           SizedBox(
                             height: 1.h,
                           ),
+                          menuController.selectedCategory != null
+                              ? Row(
+                                  children: [
+                                    CustomNetworkImage(
+                                      imageUrl: menuController
+                                          .selectedCategory.categoryImage,
+                                      size: 20.w,
+                                      isCircular: true,
+                                      borderWidth: 4.0,
+                                    ),
+                                    SizedBox(
+                                      width: 2.w,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          width: 60.w,
+                                          child: Text(
+                                            menuController
+                                                .selectedCategory.categoryName,
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: context
+                                                .textTheme.titleMedium!
+                                                .copyWith(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 60.w,
+                                          child: ReadMoreText(
+                                            menuController.selectedCategory
+                                                .categoryDescription!,
+                                            trimMode: TrimMode.Line,
+                                            trimLines: 2,
+                                            colorClickableText:
+                                                AppColors.primaryLight,
+                                            trimCollapsedText: 'Show more',
+                                            trimExpandedText: 'Show less',
+                                            moreStyle: TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
+                              : SizedBox(),
 
+                          // SizedBox(
+                          //   height: 1.h,
+                          // ),
+                          // DottedLine(
+                          //   direction: Axis.horizontal,
+                          //   alignment: WrapAlignment.center,
+                          //   lineLength: double.infinity,
+                          //   lineThickness: 1.0,
+                          //   dashLength: 4.0,
+                          //   dashColor: Colors.black,
+                          //   dashRadius: 0.0,
+                          //   dashGapLength: 4.0,
+                          //   dashGapColor: Colors.transparent,
+                          //   dashGapRadius: 0.0,
+                          // ),
+
+                          SizedBox(
+                            height: 1.h,
+                          ),
                           menuController.items.length > 0
                               ? ListView.builder(
                                   shrinkWrap: true,
