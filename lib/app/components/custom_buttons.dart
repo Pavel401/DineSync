88260ae08f7f24b33dcbd6a760240523fe74ae4d
Mabud <pavelalam401@gmail.com>
2024-12-12 00:43:55 +0530

    Add isEnabled property to button components for better state management

--- lib/app/components/custom_buttons.dart
+++ lib/app/components/custom_buttons.dart
@@ -9,24 +9,34 @@ class CafePrimaryButton extends StatelessWidget {
   final double? height;
   final String buttonTitle;
   final Function()? onPressed;
+  final bool isEnabled;
 
   CafePrimaryButton({
     required this.buttonTitle,
     required this.onPressed,
     this.width,
     this.height,
+    this.isEnabled = true,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: isEnabled ? onPressed : null,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isEnabled ? AppColors.primaryLight : Colors.black38,
+      ),
       child: Container(
         width: width ?? 80.w,
         height: height ?? 5.h,
         child: Center(
-          child: Text(buttonTitle),
+          child: Text(
+            buttonTitle,
+            style: TextStyle(
+              color: isEnabled ? Colors.white : Colors.black,
+            ),
+          ),
         ),
       ),
     );
@@ -38,24 +48,36 @@ class CafeSecondaryButton extends StatelessWidget {
   final double? height;
   final String buttonTitle;
   final Function()? onPressed;
+  final bool isEnabled;
 
   const CafeSecondaryButton({
     required this.buttonTitle,
     required this.onPressed,
     this.width,
     this.height,
+    this.isEnabled = true,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return OutlinedButton(
-      onPressed: onPressed,
+      onPressed: isEnabled ? onPressed : null,
+      style: OutlinedButton.styleFrom(
+        side: BorderSide(
+          color: isEnabled ? AppColors.primaryLight : Colors.grey,
+        ),
+      ),
       child: Container(
         width: width ?? 80.w,
         height: height ?? 5.h,
         child: Center(
-          child: Text(buttonTitle),
+          child: Text(
+            buttonTitle,
+            style: TextStyle(
+              color: isEnabled ? AppColors.primaryLight : Colors.grey,
+            ),
+          ),
         ),
       ),
     );
@@ -65,35 +87,42 @@ class CafeSecondaryButton extends StatelessWidget {
 class TurfActionButton extends StatelessWidget {
   final void Function()? onPressed;
   final IconData icon;
+  final bool isEnabled;
+
   const TurfActionButton({
     super.key,
     required this.onPressed,
     required this.icon,
+    this.isEnabled = true,
   });
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return GestureDetector(
-      onTap: onPressed,
+      onTap: isEnabled ? onPressed : null,
       child: Container(
         padding: EdgeInsets.all(1.h),
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           border: Border.all(
             width: 2,
-            color: themeProvider.isDarkMode
-                ? AppColors.primaryDark
-                : AppColors.surfaceLight,
+            color: isEnabled
+                ? (themeProvider.isDarkMode
+                    ? AppColors.primaryDark
+                    : AppColors.surfaceLight)
+                : Colors.grey,
           ),
         ),
         child: Center(
           child: Icon(
             icon,
             size: 30,
-            color: themeProvider.isDarkMode
-                ? AppColors.primaryDark
-                : AppColors.secondaryLight,
+            color: isEnabled
+                ? (themeProvider.isDarkMode
+                    ? AppColors.primaryDark
+                    : AppColors.secondaryLight)
+                : Colors.grey,
           ),
         ),
       ),
@@ -104,14 +133,14 @@ class TurfActionButton extends StatelessWidget {
 class TurfPaymentButton extends StatelessWidget {
   final double totalAmount;
   final void Function() onPressed;
-
   final bool isDisabled;
 
-  const TurfPaymentButton(
-      {super.key,
-      required this.totalAmount,
-      required this.onPressed,
-      this.isDisabled = false});
+  const TurfPaymentButton({
+    super.key,
+    required this.totalAmount,
+    required this.onPressed,
+    this.isDisabled = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -119,9 +148,7 @@ class TurfPaymentButton extends StatelessWidget {
     return SizedBox(
       height: 8.h,
       child: ElevatedButton(
-        onPressed: () {
-          onPressed();
-        },
+        onPressed: isDisabled ? null : onPressed,
         style: ElevatedButton.styleFrom(
           backgroundColor:
               isDisabled ? theme.disabledColor : theme.colorScheme.primary,
@@ -142,14 +169,27 @@ class TurfPaymentButton extends StatelessWidget {
                 children: [
                   Text(
                     "Total Amount",
+                    style: TextStyle(
+                      color: isDisabled
+                          ? Colors.grey
+                          : theme.colorScheme.onPrimary,
+                    ),
                   ),
                   Text(
                     "₹$totalAmount",
+                    style: TextStyle(
+                      color: isDisabled
+                          ? Colors.grey
+                          : theme.colorScheme.onPrimary,
+                    ),
                   ),
                 ],
               ),
               Text(
                 "Continue",
+                style: TextStyle(
+                  color: isDisabled ? Colors.grey : theme.colorScheme.onPrimary,
+                ),
               ),
             ],
           ),
