4c803d02c393824da7a3622e2385564f9e37ebf6
Mabud <pavelalam401@gmail.com>
2024-12-12 00:44:00 +0530

    Add isOrderEmpty property to WaiterOrderController for order state management

--- lib/app/modules/Waiter App/New Order/controller/new_order_controller.dart
+++ lib/app/modules/Waiter App/New Order/controller/new_order_controller.dart
@@ -4,12 +4,15 @@ import 'package:get/get.dart';
 class WaiterOrderController extends GetxController {
   Map<FoodItem, int> orderItems = {};
 
+  RxBool isOrderEmpty = true.obs;
+
   void addOrderItem(FoodItem foodItem) {
     if (orderItems.containsKey(foodItem)) {
       orderItems[foodItem] = orderItems[foodItem]! + 1;
     } else {
       orderItems[foodItem] = 1;
     }
+    isOrderEmpty.value = false;
     update();
   }
 
@@ -20,6 +23,9 @@ class WaiterOrderController extends GetxController {
       } else {
         orderItems.remove(foodItem);
       }
+    }
+    if (orderItems.isEmpty) {
+      isOrderEmpty.value = true;
     }
     update();
   }
